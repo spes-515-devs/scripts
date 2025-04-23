304ddaf3858bc2aca0134bc8fa40ce799509c4a0
tanvirr007 <43490582+tanvirr007@users.noreply.github.com>
2025-04-23 15:07:41 +0600

    spes: Add spes.sh
    - Script to clone all required src for spes-5.15 setup using shallow clones
    
    Signed-off-by: tanvirr007 <43490582+tanvirr007@users.noreply.github.com>

---
+++ spes.sh
@@ -0,0 +1,6 @@
+git clone -b Main --depth=1 https://github.com/spes-515-devs/android_kernel_xiaomi_sm6225-5.15.git kernel/xiaomi/sm6225-5.15
+git clone -b main --depth=1 https://github.com/spes-515-devs/android_kernel_xiaomi_sm6225-devicetrees.git kernel/xiaomi/sm6225-devicetrees
+git clone -b main --depth=1 https://github.com/spes-515-devs/android_kernel_xiaomi_sm6225-modules.git kernel/xiaomi/sm6225-modules
+git clone -b lineage-20 --depth=1 https://github.com/spes-515-devs/android_device_xiaomi_spes-5.15 device/xiaomi/spes
+git clone -b lineage-20 --depth=1 https://github.com/spes-515-devs/android_vendor_xiaomi_spes-5.15.git vendor/xiaomi/spes
+git clone -b lineage-20 --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
